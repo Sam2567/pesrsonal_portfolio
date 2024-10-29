79026818d8302b6a0bf38bf49fdd8dd77007d2de
Victoria 2451 <qscrev98@gmail.com>
2024-10-29 19:54:29 +1030

    Add About Me

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,9 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png"></img>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,13 @@
+import AboutMe from "../AboutMe";
+import MySkills from "../MySkills";
 import ProfileSection from "../ProfileSection";
 
 export default function Home() {
   return (
     <>
       <ProfileSection />
+      <MySkills />
+      <AboutMe />
     </>
   );
 }
