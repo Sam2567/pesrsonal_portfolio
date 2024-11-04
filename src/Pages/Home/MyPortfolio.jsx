bf3d3ce30d619e264ec0e1ed9cf7e6520a8bb602
Victoria 2451 <qscrev98@gmail.com>
2024-11-04 15:29:33 +1030

    Finish the portfolio section

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -1,16 +1,16 @@
-import data from "../../data/index.json"
+import data from "../../data/index.json";
 
-export default function MyPortfolio () {
-    return <section className="portfolio--section"
-    id="MyPortfolio">
-        <div className="portfolio--container-box">
-            <div className="portfolio--container">
-                <p className="sub--title">Recent Projects</p>
-                <h2 className="section--heading">My Portfolio</h2>
-            </div>
-            <div>
-                <button className="btn btn-github">
-                <svg
+export default function MyPortfolio() {
+  return (
+    <section className="portfolio--section" id="MyPortfolio">
+      <div className="portfolio--container-box">
+        <div className="portfolio--container">
+          <p className="sub--title">Recent Projects</p>
+          <h2 className="section--heading">My Portfolio</h2>
+        </div>
+        <div>
+          <button className="btn btn-github">
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
               height="32"
@@ -25,23 +25,23 @@ export default function MyPortfolio () {
               />
             </svg>
             Visit My GitHub
-                </button>
-            </div>
+          </button>
         </div>
-        <div className="portfolio--section--contrainer">
-        {data?.portfolio.map((item, index) => (
-            <div key={index} className="portfolio--section--card">
-                <div className="portfolio--section--img">
-                    <img src={item.src} alt="Place holder"/>
-                </div>
-                <div className="portfolio--section--card--content">
-                    <div>
-                        <h3 className="portfolio--section--title">
-                            {item.title}
-                            <p className="text-md">{item.description}</p>
-                        </h3>
-                    </div>
-                    <p className="text-sm portfolio--link">
+      </div>
+      <div className="portfolio--section--container">
+        {data?.portfolio?.map((item, index) => (
+          <div key={index} className="portfolio--section--card">
+            <div className="portfolio--section--img">
+              <img src={item.src} alt="Placeholder" />
+            </div>
+            <div className="portfolio--section--card--content">
+              <div>
+                <h3 className="portfolio--section--title">
+                  {item.title}
+                  <p className="text-md">{item.description}</p>
+                </h3>
+              </div>
+              <p className="text-sm portfolio--link">
                 {item.link}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -59,9 +59,10 @@ export default function MyPortfolio () {
                   />
                 </svg>
               </p>
-                </div>
             </div>
+          </div>
         ))}
-        </div>
+      </div>
     </section>
+  );
 }
